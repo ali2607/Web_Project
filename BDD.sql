1dfc088576b30243d621920b5bb3ddbd3a2b7641
Ali <alilena999@gmail.com>
2022-12-14 16:35:31 +0100

    [sql] test ajout bdd

---
+++ BDD.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 14 déc. 2022 à 16:29
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet_web_bdd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `testbdd`
+--
+
+CREATE TABLE `testbdd` (
+  `A` int(7) NOT NULL,
+  `B` varchar(8) NOT NULL,
+  `C` int(9) NOT NULL,
+  `D` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `testbdd`
+--
+
+INSERT INTO `testbdd` (`A`, `B`, `C`, `D`) VALUES
+(1, 'a', 1, 'b');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `testbdd`
+--
+ALTER TABLE `testbdd`
+  ADD PRIMARY KEY (`A`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `testbdd`
+--
+ALTER TABLE `testbdd`
+  MODIFY `A` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
