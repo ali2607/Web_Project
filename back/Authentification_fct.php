037b4865f042634da0405e8b3634a43585186ece
Elie <eliefares02@yahoo.com>
2023-03-07 22:26:23 +0100

    sign up issue

--- back/Authentification_fct.php
+++ back/Authentification_fct.php
@@ -14,40 +14,48 @@
             Logout();
             break;
     }
-    }    
+    }
 
-    // Enregistrement d'un nouvel utilisateur
-    function SignUp()
-    {
-        include('../database/db.php');
+// Enregistrement d'un nouvel utilisateur
+function SignUp()
+{
+    include('../database/db.php');
 
-        //echo $_POST["SignUp_Username"];
+    if (isset($_POST["SignUp_Username"]) && isset($_POST["SignUp_Password"])) {
+        $pseudo = $_POST['SignUp_Username'];
+        $mdp = $_POST['SignUp_Password'];
+
+        $query = "SELECT * FROM _user WHERE username = '$pseudo' AND password = '$mdp'";
+
+        $result = mysqli_query($conn, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            // User already exists
+            session_start();
+            $_SESSION['errormsg'] = "User already exists";
+            header("Location: ../front/SignUp.php");
+            exit();
+        }
+
         if (empty($_POST["SignUp_Username"])) {
             session_start();
             $_SESSION['errormsg'] = "username is empty";
             header("Location: ../front/SignUp.php");
-                 }
-    else if (empty($_POST["SignUp_Password"])) {
-        session_start();
-        $_SESSION['errormsg'] = "password is empty";
-        header("Location: ../front/SignUp.php");
-    }
-        else if (isset($_POST["SignUp_Username"]) && isset($_POST["SignUp_Password"])) {
-            $pseudo = $_POST['SignUp_Username'];
-            $mdp = $_POST['SignUp_Password'];
-    
-            if($_POST["SignUp_Password"] != $_POST["SignUp_ConfirmPassword"] )
-            {
-                session_start();
-                $_SESSION['errormsg'] = "Password and confirmation password incorrect";
-                header("Location: ../front/SignUp.php");
-            }
-            else
-            {
+            exit();
+        } else if (empty($_POST["SignUp_Password"])) {
+            session_start();
+            $_SESSION['errormsg'] = "password is empty";
+            header("Location: ../front/SignUp.php");
+            exit();
+        } else if ($_POST["SignUp_Password"] != $_POST["SignUp_ConfirmPassword"]) {
+            session_start();
+            $_SESSION['errormsg'] = "Password and confirmation password incorrect";
+            header("Location: ../front/SignUp.php");
+            exit();
+        } else {
+            $query = "INSERT INTO _user (username, password) VALUES ('$pseudo', '$mdp')";
 
-                $query = "INSERT INTO _user (username, password) VALUES ('$pseudo', '$mdp')";
-                    
-                if (mysqli_query($conn, $query)) {
+            if (mysqli_query($conn, $query)) {
                 // Enregistrement réussi
                 $query = "SELECT idUser from _user where username = '$pseudo' and password = '$mdp'";
                 $result = mysqli_query($conn, $query);
@@ -63,18 +71,16 @@
                 mysqli_query($conn, $query);
                 $query = "INSERT INTO _score (idUser, idJeu) VALUES ('$idUser', '2')";
                 mysqli_query($conn, $query);
-                    header("Location: ../front/Login.php");
-                } else {
-                    // Enregistrement échoué
-                    echo "Error: " . $query . "<br>" . mysqli_error($conn);
-
-                    
-                    }
-                }
+                header("Location: ../front/Login.php");
+            } else {
+                // Enregistrement échoué
+                echo "Error: " . $query . "<br>" . mysqli_error($conn);
             }
-
-        mysqli_close($conn);
+        }
     }
+
+    mysqli_close($conn);
+}
 
     // Connexion d'un utilisateur existant
     function Login()
